fd09bd58d035be5eaf1fb88f93aa5bdb22247b7f
KBY-TECH <soccer_by@naver.com>
2020-08-10 16:30:04 +0900

    ebook/model.py eBook 모델 작성 ebook/view.py 함수 수정
    ==> 데이터베이스 pdf 파일을 저장하여 가져오는 것으로 변경.
    작성자: KBY_TECH

--- smartEdu/eBook/models.py
+++ smartEdu/eBook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Ebook(models.Model):
+    eNo = models.IntegerField(db_column='eNo',db_index=True, primary_key=True)
+    eName = models.CharField(db_column='eName', max_length=128)
+    eData = models.CharField(db_column='eData', max_length=1000)
+
+    class Meta:
+        managed = False
+        db_table = 'Ebook'
+
+
+    def __str__(self):
+        return "eNo : " + self.eNo + " eName : " + self.Name + " eData : " + self.Data
