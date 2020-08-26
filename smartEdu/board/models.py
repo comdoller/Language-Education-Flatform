a09fe0b7ee967c206b5b1194229fed37315cfab4
comdoller <nearermg@gmail.com>
2020-08-26 18:42:02 +0900

    0252d85 복구, UI 다시 반영

--- smartEdu/board/models.py
+++ smartEdu/board/models.py
@@ -13,11 +13,8 @@ class Board(models.Model):
     filename = models.CharField(null=True, default="", max_length=500)
     filesize = models.IntegerField(default=0)
     down = models.IntegerField(default=0)
-    #수정 생성 따로 추기
     created = models.DateTimeField(default=datetime.now, blank=True)
     updated = models.DateTimeField(default=datetime.now, blank=True)
-    class Meta:
-        ordering = ['-created', '-updated']
 
     def hit_up(self):
         self.hit += 1
